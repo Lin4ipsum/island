47b5fe719a665f157398435e02bb8efc8ddcccf7
Linda <lin.m.gonzalez@gmail.com>
2013-02-07 16:58:57 -0500

    initial commit and added pg and heroku san gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Island::Application.config.secret_token = '008ab0359dedb32e63aa7f6b20488dda0526d9367eb7a741ca08dcd814ec6bf37926dbc2bf8911bf200257ed9d2f03e9c649ad0c00c325b5cfdf1eac4bd85fe5'
