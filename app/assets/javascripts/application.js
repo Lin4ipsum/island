066eb3409178f43eb3769880f1a0881b98c870b9
Linda <lin.m.gonzalez@gmail.com>
2013-02-08 19:45:50 -0500

    added jquery code to island.js and added island.js
    and jquery ui to the app.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require island
+//= require jquery-ui
 //= require_tree .
--- app/assets/javascripts/island.js
+++ app/assets/javascripts/island.js
@@ -3,3 +3,9 @@ $(document).ready(function(){
     $(this).animate({top:'+=100px'},500);
  }); 
 });
+
+$(document).ready(function(){
+  $('.coconut').mouseenter(function(){
+    $(this).effect('shake', 'slow');
+  })
+});
