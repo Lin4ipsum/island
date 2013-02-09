5212de929fb5de0081d68adebc28b3ee9dacabc9
Linda <lin.m.gonzalez@gmail.com>
2013-02-08 23:42:13 -0500

    added crab, starfish images and jquery
    added google font

--- app/assets/javascripts/island.js
+++ app/assets/javascripts/island.js
@@ -2,10 +2,20 @@ $(document).ready(function(){
  $('.sun').mouseenter(function(){
     $(this).animate({top:'+=100px'},500);
  }); 
-});
 
-$(document).ready(function(){
   $('.coconut').mouseenter(function(){
     $(this).effect('shake', 'slow');
+  });
+
+  $('.starfish').click(function(){
+      $(this).toggle('explode');
+  });
+
+  $('.crab').click(function(){
+    $(this).effect('shake', 'slow');
   })
+
 });
+
+
+
